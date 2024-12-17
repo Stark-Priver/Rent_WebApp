b96c8152818e3203d0ec09e6743f80cb12b26a09
Priver <priverdeprince@yahoo.com>
2024-12-17 13:19:24 +0300

    Created some new Feature apps

--- rent_backend/rent_backend/settings.py
+++ rent_backend/rent_backend/settings.py
@@ -40,6 +40,12 @@ INSTALLED_APPS = [
     'home',
     "myaccount",
     "post",
+    'messages',
+    'rest_framework',
+    'rest_framework.authtoken',
+    'corsheaders',
+    'transactions',
+    
 ]
 
 MIDDLEWARE = [
